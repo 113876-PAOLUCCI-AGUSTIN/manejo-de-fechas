cba8cbc0f60de34cce650a7a8c5b3f308a63f90b
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-24 07:19:23 -0300

    Probando metodos.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LocalDate fechaActual = LocalDate.now(); // Fecha actual
+
+        System.out.println(fechaActual); // Fecha de hoy
+        System.out.println(fechaActual.getDayOfWeek()); // Dia de la semana
+        System.out.println(fechaActual.getYear()); // Obtener el año, mes, dia
+        System.out.println(fechaActual.getMonth()); // Obtener el año, mes, dia
+        System.out.println(fechaActual.getDayOfMonth()); // Obtener el año, mes, dia
     }
 }
